538f3c8c5181a2c699bfed371eb90ee61caafb5b
Shaun <shaun@Shauns-Air.lan>
2024-02-20 02:50:22 +1300

    somewhat working, not sure why it trains slower than the python version

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{error::Error, os::unix::process::parent_id, usize};
 
 type MatrixVec = Vec<DMatrix<f64>>;
 
-const SAMPLES: f64 = 10000.0;
+const SAMPLES: f64 = 40000.0;
 
 fn generate_random_number() -> f64 {
     let mut rng = thread_rng();
@@ -51,32 +51,43 @@ fn init(weights: &mut MatrixVec, biases: &mut MatrixVec, sizes: &[usize]) {
     }
 }
 
-fn feed_forward(weights: &mut MatrixVec,biases: &mut MatrixVec, x_input: &DMatrix<f64> ) -> (MatrixVec, MatrixVec){
+fn feed_forward(weights: &MatrixVec,biases: &MatrixVec, x_input: &DMatrix<f64> ) -> (MatrixVec, MatrixVec){
     let mut activations: MatrixVec = Vec::new();
     let mut inputs: MatrixVec = Vec::new();
     activations.push(x_input.clone());
 
     for (w, b) in weights.iter().zip(biases.iter()) {
-        // println!("{:?}", b.shape());
+        // println!("w shape {:?} b shape{:?}", w.shape(), b.shape());
         let  mut z = w * activations.last().expect("could not get last from feed_forward");
-        //add biases, need to do col by cols as z is shape (depth, samples) and b is shape (depth, 1)
+        // add biases, need to do col by cols as z is shape (depth, samples) and b is shape (depth, 1)
         for col_index in 0..z.ncols() {
-                let mut col = z.column_mut(col_index);
-                col += b;
-            }
+                let col = DMatrix::from_column_slice(z.nrows(), 1, z.column(col_index).as_slice());
+                // let mut col = z.column_mut(col_index);
+                // let col = z.column(col_index).as_slice();
+                for i in (0..col.len()-1) {
+                     z[(i, col_index)] += col[i] ;
+                }
+        }
         activations.push(z.map(|x| sigmoid(x)));
         inputs.push(z);
     }
+    // println!("activations len {:?} inputs len {:?}", activations.len(), inputs.len());
     (activations, inputs)
 }
 
 fn one_hot(input: &DMatrix<f64>) -> DMatrix<f64> {
     let mut output: DMatrix<f64> = DMatrix::repeat(10, input.len(), 0.0);
-    for (row, col) in input.iter().zip(1..input.len()) {
-        output[(row.clone() as usize,col)] = 1.0;
+    // println!("input size {:?}", input.len());
+    for col in 0..input.len() {
+        // println!("col {col}");
+        let val = input[col] as usize;
+        output[(val,col)] = 1.0;
+        // println!("target {} output one hot {:?}", input[col], output.column(col).as_slice());
     }
     output
+
 }
+
 
 fn back_prop(activations: &MatrixVec, inputs: MatrixVec,weights: &MatrixVec, targets: &DMatrix<f64>) -> (MatrixVec, MatrixVec) {
     let mut dW: MatrixVec = Vec::new();
@@ -84,25 +95,28 @@ fn back_prop(activations: &MatrixVec, inputs: MatrixVec,weights: &MatrixVec, tar
     let mut dZ: MatrixVec = Vec::new();
     let one_hot = one_hot(targets);
 
-    let size = activations.len();
-    let z = &activations[size-1] - one_hot;
+    let size = weights.len();
+    let z = activations.last().expect("should not be empty") - one_hot;
     dZ.push(z);
     // println!("activations len {:?}", activations.len());
 
-    for i in (0..(size-1)).rev() {
+    for i in (0..size).rev() {
+        // println!("size {} at {}", size, i);
         // println!("activations {} shape {:?}", i, activations[i].shape());
         let dz = dZ.last().expect("backprop 1");
-        let dw = 1.0/SAMPLES * dz * activations[i].transpose();
+        // println!("weights shape {:?} dz shape {:?}", weights[i].shape(), dz.shape());
+        let dw =(dz * activations[i].transpose()) * 1.0/SAMPLES;
         // println!("dw {} shape {:?}", i, dw.shape());
         dW.insert(0, dw);
         let row_sums = dz.row_iter().map(|row| row.sum()).collect::<Vec<f64>>();
         let db = DMatrix::from_column_slice(row_sums.len(), 1, &row_sums);
-        let db_scaled = db * (1.0/SAMPLES);
+        let db_scaled = db * 1.0/SAMPLES;
         dB.insert(0, db_scaled);
 
         if i > 0 {
            let dz = weights[i].transpose() * dz;
-           let sigmoids = inputs[i].map(|x| sigmoid_prime(x));
+           let sigmoids = inputs[i-1].map(|x| sigmoid_prime(x));
+           // println!("sigmoids shape {:?} dz shape {:?}", sigmoids.shape(), dz.shape());
            dZ.push(dz.component_mul(&sigmoids));
         }
 
@@ -120,34 +134,35 @@ fn get_accurracy(activations:&MatrixVec, targets:&DMatrix<f64>) {
     let mut total = 0;
 
     // Iterate over each row of the output matrix
-        for (i, row) in output.row_iter().enumerate() {
-            let max_index = row.iter().enumerate().max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap()).map(|(index, _)| index);
+    for (i, row) in output.row_iter().enumerate() {
+        let max_index = row.iter().enumerate().max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap()).map(|(index, _)| index);
 
-            let target_value = targets[(i, 0)] as usize;
-            if let Some(max_index) = max_index {
-                if max_index == target_value {
-                    total += 1;
-                }
+        let target_value = targets[(i, 0)] as usize;
+        if let Some(max_index) = max_index {
+            if max_index == target_value {
+                total += 1;
             }
         }
+    }
         println!("accuracy: {}", (total as f64)/SAMPLES);
 }
 
 fn SGD(x_input: DMatrix<f64>, targets: DMatrix<f64>) {
-    let sizes = [784, 10, 10];
+    let sizes = [784, 64, 10];
     let mut weights: Vec<DMatrix<f64>> = Vec::new();
     let mut biases: Vec<DMatrix<f64>> = Vec::new();
+    let l_r:f64 = 3.0;
     init(&mut weights, &mut biases, &sizes);
-    let epochs = 50;
+    let epochs = 500;
     for i in 0..epochs {
-        let (activations, inputs) = feed_forward(&mut weights, &mut biases, &x_input);
+        let (activations, inputs) = feed_forward(&weights, &biases, &x_input);
         let (dW, dB) = back_prop(&activations, inputs, &weights, &targets);
         // println!("big dW shape {:?}", dW[0].shape());
         //update
         for i in 0..(sizes.len()-1) {
             // println!("{:?} {:?}", weights[i].shape(), dW[i].shape());
-            weights[i] = &weights[i] - &dW[i] * (3.0);
-            biases[i] = &biases[i] - &dB[i] * (3.0)
+            weights[i] = &weights[i] - (&dW[i] * (l_r));
+            biases[i] = &biases[i] - (&dB[i] * (l_r))
         }
         if i % 10 == 0 {
             get_accurracy(&activations, &targets);
@@ -175,8 +190,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let x_train = DMatrix::from_column_slice(data[0].len(), data.len(), &flattened);
 
     println!("Data loaded. Begining training!");
+    println!("X_train lenght {}", x_train.len()/784);
 
-    SGD(x_train, DMatrix::from_vec(y_train.len(), 1, y_train));
+    SGD(x_train/255.0, DMatrix::from_vec(y_train.len(), 1, y_train));
 
    // let (activations, _inputs) = feed_forward(&mut weights, &mut biases, &x_train);
 
